1182c6371097b46d243f84e1a0f904f8c1b07dc9
Lucas Seguessi <lucas.seguessi@gmail.com>
2024-02-05 20:34:31 -0300

    Add CORS

--- Program.cs
+++ Program.cs
@@ -15,6 +15,19 @@ builder.Services.AddControllers()
             options.SuppressModelStateInvalidFilter = true;
         });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Development", builder => 
+                       builder
+                      .AllowAnyOrigin()
+                      .AllowAnyMethod()
+                      .AllowAnyHeader());
+    options.AddPolicy("Production", builder =>
+                    builder.WithOrigins("https://localhost:9000")
+                    .WithMethods("POST")
+                    .AllowAnyHeader());
+});
+
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApiDbContext>();
@@ -84,6 +97,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("Development");
+} else
+{
+    app.UseCors("Production");
 }
 
 app.UseHttpsRedirection();
